1dfe46f99753e92e41273e9ebf28becf01b5e974
Glitch (numerous-hill-archduke) <none>
2021-02-20 03:14:16 +0000

    ⏳🏖 Checkpoint ./server.js:28550310/904

--- server.js
+++ server.js
@@ -10,33 +10,13 @@ app.route('/')
 		  res.sendFile(process.cwd() + '/views/index.html');
     })
 
-const MonthChoice=['January','February','Month','April','May','June','July','August','September','October','November','December']
 app.use('/assets',express.static('assets'))
 
+var str = '1451001600000';
+var i = new Date().toUTCString();
+var a = new Date(1451001600000)
+console.log(a)
+console.log(i)
 
-app.get('/:parsed', function (req, res) {
-  
-  if (isNaN(req.params.parsed)) {
-    const time=new Date(req.params.parsed)
-    if (typeof time=='number') {
-      res.json({
-        unix: time/1000,
-        natural:MonthChoice[time.getMonth()]+' '+time.getDate()+', '+time.getFullYear()
-    })
-    } else {
-      res.json({
-        unix: null,
-        time: null
-      })
-    }
-  } else {
-    const d = new Date(req.params.parsed*1000)
-    res.json({
-      unix: req.params.parsed,
-      natural: MonthChoice[d.getMonth()]+' '+d.getDate()+', '+d.getFullYear()
-    })
-  }
-  console.log(req.url)
-})
 
 app.listen(process.env.PORT || 8080)
