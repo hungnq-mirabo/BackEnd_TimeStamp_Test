80a16215701e7312ddf311ef0e006fc066f55eda
Glitch (numerous-hill-archduke) <none>
2021-02-20 03:26:09 +0000

    🚒💕 Checkpoint ./server.js:28550310/319

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ var str = '1451001600000';
 var i = new Date().toUTCString();
 var a = new Date(1451001600000)
 console.log(a)
+console.log(a.valueOf(a))
+console.log(Date.parse(a))
 console.log(i)
 
+app.get('/api/timestamp/', (req, res) => {
+  res.json(req.timestamp)
+})
 
 app.listen(process.env.PORT || 8080)
