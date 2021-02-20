a0755a55e7908a9586f5813af40cd0f74ae100de
Glitch (numerous-hill-archduke) <none>
2021-02-20 04:35:28 +0000

    🌷👐 Checkpoint ./server.js:28550310/1481

--- server.js
+++ server.js
@@ -12,11 +12,13 @@ app.route('/')
 
 app.use('/assets',express.static('assets'))
 
+var a = new Date("2015-10-10")
+console.log(a)
+
 app.get('/api/timestamp/', (req, res) => {
   let today = new Date()
   let date_UTC = today.toUTCString();
-  let date_Unix = Date.valueOf(today);
-  console.log(today,date_UTC)
+  let date_Unix = today.valueOf();
   res.json({"unix":date_Unix,"utc":date_UTC})
 })
 
@@ -26,17 +28,20 @@ app.get('/api/timestamp/', (req, res) => {
 
 app.get('/api/timestamp/:date_string', (req, res) => {
   var date_string = req.params.date_string;
-  if (!date_string) {return res.send("ko co ngay")}
+
   let regex_date = /^(\d{4})-(\d{2})-(\d{2})/;
-  let regex_unix = /\d{4,}/;
-  if (regex_date.test(date_string)) {
-    let date_Unix = Date.parse(date_string);
-    let date_UTC = new Date(date_Unix).toUTCString();
-    res.json({"unix":date_Unix,"utc":date_UTC})
-  }
-  else if (regex_unix.test(date_string)) {
+  let regex_unix = /\d{5,}/;
+  
+  if (regex_unix.test(date_string)) {
     let date_Unix = parseInt(date_string);
     let date_UTC = new Date(date_Unix).toUTCString();  
+    res.json({"unix":date_Unix,"utc":date_UTC})
+  } else {
+    
+  }
+  else if (regex_date.test(date_string)) {
+    let date_Unix = Date.parse(date_string);
+    let date_UTC = new Date(date_Unix).toUTCString();
     res.json({"unix":date_Unix,"utc":date_UTC})
   }
   else {
